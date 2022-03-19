184ebcdfc14d68ab55529defca088de27dc4a912
kang-sumin <54437758+kang-sumin@users.noreply.github.com>
2022-03-19 20:02:26 +0900

    Day19_Mar
    모두의 C언어 19일 코딩

---
+++ Function_W.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+func10(int x); //함수 선언, 출력자료형이 없는 함수임
+
+int main() {
+
+	int a = 1, b = 3, c = 5;
+
+	//함수 호출
+	func10(a);
+	func10(b);
+	func10(c);
+
+	return 0;
+}
+
+//함수 구현(함수 몸체)
+func10(int x) {
+	x = x * 10;
+	printf("10배가 된 값은 %d입니다.\n", x);
+}
