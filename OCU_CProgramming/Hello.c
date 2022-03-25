5975db26496139120464395d8db75f6f306b1f92
kang-sumin <54437758+kang-sumin@users.noreply.github.com>
2022-03-26 00:08:13 +0900

    C 프로그래밍
    2주차 3교시 실습 코드

---
+++ OCU_CProgramming/Hello.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void) {
+	//printf() : 라이브러리 함수
+	printf("Hello World");
+
+	return 0;
+}
---
+++ OCU_CProgramming/calc.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+
+	printf("2+3=%d\n", 2 + 3); //µ¡¼À addition
+	printf("2-3=%d\n", 2 - 3); //»¬¼À subtraction
+	printf("2*3=%d\n", 2 * 3); //°ö¼À multiplication
+	printf("2/3=%d\n", 2 / 3); //³ª´©±â division
+	
+	return 0;
+}
