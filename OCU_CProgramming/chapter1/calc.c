3f36e7684ee3153c3d1ec7cf019c4a4d1e25e4c3
kang-sumin <54437758+kang-sumin@users.noreply.github.com>
2022-05-26 20:17:07 +0900

    Update calc.c
    사칙연산 출력하기

--- OCU_CProgramming/chapter1/calc.c
+++ OCU_CProgramming/chapter1/calc.c
@@ -1,7 +1,8 @@
 ﻿#include <stdio.h>
 
 int main(void) {
-
+	
+	//사칙연산 출력하기
 	printf("2+3=%d\n", 2 + 3); //덧셈 addition
 	printf("2-3=%d\n", 2 - 3); //뺄셈 subtraction
 	printf("2*3=%d\n", 2 * 3); //곱셈 multiplication
