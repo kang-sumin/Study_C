8f543646e24ad72032c6c15eec62274b2e1fac2c
kang-sumin <54437758+kang-sumin@users.noreply.github.com>
2022-05-30 23:04:32 +0900

    Update calc.c
    edit comment

--- OCU_CProgramming/chapter1/calc.c
+++ OCU_CProgramming/chapter1/calc.c
@@ -2,7 +2,7 @@
 
 int main(void) {
 	
-	//사칙연산 출력하기
+	//사칙연산 출력문
 	printf("2+3=%d\n", 2 + 3); //덧셈 addition
 	printf("2-3=%d\n", 2 - 3); //뺄셈 subtraction
 	printf("2*3=%d\n", 2 * 3); //곱셈 multiplication
